ee22080755c80376e881e26b9e887efad7587541
hyeongeun <hhg7899@gmail.com>
2023-08-25 10:19:04 +0900

    snackbar 기능 완료

--- src/plan/DatePicker.jsx
+++ src/plan/DatePicker.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { DateRange } from 'react-date-range';
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
@@ -12,7 +12,6 @@ import AppendCard from '../AppendCard';
 import AlertTitle from '@mui/material/AlertTitle';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
-import { Button } from '@mui/material';
 
 function DatePicker({ selectedItems }) {
     const maxDate = 5;
@@ -27,13 +26,46 @@ function DatePicker({ selectedItems }) {
 
     const [expanded, setExpanded] = useState('panel0');
     const [snackbarOpen, setSnackbarOpen] = useState(false);
+    const [accordions, setAccordions] = useState([]);
+
+    // useEffect 사용해서 re-renders infinite Loop 방지
+    useEffect(() => {
+        const updatedAccordions = [];
+
+        if (state[0].startDate && state[0].endDate) {
+            const startDate = new Date(state[0].startDate);
+            const endDate = new Date(state[0].endDate);
+            const dayInMillis = 24 * 60 * 60 * 1000; // 하루의 밀리초
+            const dateRangeInDays = (endDate - startDate) / dayInMillis + 1;
+
+            if (dateRangeInDays > maxDate) {
+                handleSnackbarOpen();
+            } else {
+                for (let i = 0; i < dateRangeInDays; i++) {
+                    updatedAccordions.push(
+                        // 아코디언을 일정 수 만큼 생성
+                        <Accordion key={i} expanded={expanded === `panel${i}`} onChange={handleChange(`panel${i}`)}>
+                            <AccordionSummary
+                                aria-controls={`panel${i}d-content`}
+                                id={`panel${i}d-header`}
+                                expandIcon={<ExpandMoreIcon />}
+                            >
+                                <Typography>DAY {i + 1}</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails style={{ padding: 0 }}>
+                                <AppendCard selectedItems={selectedItems} day={i + 1} />
+                            </AccordionDetails>
+                        </Accordion>
+                    );
+                }
+            }
+        }
+
+        setAccordions(updatedAccordions);
+    }, [state, expanded, selectedItems]);
 
     const handleChange = (panel) => (event, newExpanded) => {
         setExpanded(newExpanded ? panel : false);
-    };
-
-    const handleClick = (newState) => () => {
-        setState({ ...state, ...newState, open: true });
     };
 
     const handleSnackbarOpen = () => {
@@ -47,35 +79,6 @@ function DatePicker({ selectedItems }) {
         setSnackbarOpen(false);
     };
 
-    const accordions = [];
-
-    if (state[0].startDate && state[0].endDate) {
-        const startDate = new Date(state[0].startDate);
-        const endDate = new Date(state[0].endDate);
-        const dayInMillis = 24 * 60 * 60 * 1000; // 하루의 밀리초
-        const dateRangeInDays = (endDate - startDate) / dayInMillis + 1;
-
-        if (dateRangeInDays > maxDate) {
-            handleSnackbarOpen();
-        } else {
-            for (let i = 0; i < dateRangeInDays; i++) {
-                accordions.push(
-                    <Accordion key={i} expanded={expanded === `panel${i}`} onChange={handleChange(`panel${i}`)}>
-                        <AccordionSummary
-                            aria-controls={`panel${i}d-content`}
-                            id={`panel${i}d-header`}
-                            expandIcon={<ExpandMoreIcon />}
-                        >
-                            <Typography>{i + 1} DAY</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails style={{ padding: 0 }}>
-                            <AppendCard selectedItems={selectedItems} day={i + 1} />
-                        </AccordionDetails>
-                    </Accordion>
-                );
-            }
-        }
-    }
     return (
         <div>
             <DateRange
@@ -95,8 +98,13 @@ function DatePicker({ selectedItems }) {
             <h3>선택한 여행지</h3>
             <hr />
             {accordions}
-            <Button onClick={handleClick({ vertical: 'top', horizontal: 'center' })}>Top-Center</Button>
-            <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={handleSnackbarClose}>
+            {/* 일정이 5일을 초과하면 경고창 띄우기 */}
+            <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={4000}
+                onClose={handleSnackbarClose}
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+            >
                 <MuiAlert onClose={handleSnackbarClose} severity="warning" sx={{ width: '100%' }}>
                     <AlertTitle>Warning</AlertTitle>
                     선택한 날짜 범위는 {maxDate}일을 초과할 수 없습니다.
