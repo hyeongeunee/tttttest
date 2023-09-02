2c9227e8a27ff4f3071299af56387186b1e7ab3b
hyeongeun <hhg7899@gamil.com>
2023-09-02 10:18:11 +0900

    아코디언 관련 + 지도 관련 수정필요

--- src/plan/CreatePlanner.jsx
+++ src/plan/CreatePlanner.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
+import { Grid } from "@mui/material";
 import "./CreatePlanner.css";
 import DatePicker from "./DatePicker";
 import Map from "./Map";
 import TourCard from "./TourCard";
-import { Button, Grid } from "@mui/material";
-import { Link } from "react-router-dom";
+// import AppendCard from './AppendCard';
 
-function Test() {
+function CreatePlanner() {
   const [selectedItems, setSelectedItems] = useState([]);
+  const [selectedIndex, setSelectedIndex] = useState(0);
   const [expanded, setExpanded] = useState("panel0");
 
   const handleChange = (panel) => (event, newExpanded) => {
@@ -15,41 +16,31 @@ function Test() {
   };
 
   return (
-    <>
-      <div className="plan-header">
-        <p className="kor-title">대한민국 서울</p>
-        <p className="eng-title">SEOUL</p>
-      </div>
-      <div className="TestContainer">
-        <Grid container className="gridContainer">
-          <Grid item className="leftbar" xs={12} sm={2}>
-            <DatePicker
-              expanded={expanded}
-              handleChange={handleChange}
-              selectedItems={selectedItems}
-              setSelectedItems={setSelectedItems}
-            />
-          </Grid>
-          <Grid item className="maparea" xs={12} sm={8}>
-            <Map selectedItems={selectedItems} />
-          </Grid>
-          <Grid item className="rightbar" xs={12} sm={2}>
-            <TourCard
-              setSelectedItems={setSelectedItems}
-              selectedItems={selectedItems}
-            />
-          </Grid>
+    <div className="PlannerContainer">
+      <Grid container className="gridContainer">
+        <Grid item className="leftbar" xs={12} sm={2}>
+          <DatePicker
+            expanded={expanded}
+            handleChange={handleChange}
+            selectedItems={selectedItems}
+            setSelectedIndex={setSelectedIndex}
+            setSelectedItems={setSelectedItems}
+          />
+          {/* <AppendCard selectedItems={selectedItems} setSelectedItems={setSelectedItems} /> */}
         </Grid>
-      </div>
-      <div className="planBtn">
-        <Link to="/plan/detail">
-          <Button variant="contained" sx={{ backgroundColor: "#8181F7" }}>
-            일정 생성하기
-          </Button>
-        </Link>
-      </div>
-    </>
+        <Grid item className="maparea" xs={12} sm={8}>
+          {/* <Map selectedItems={selectedItems} /> */}
+        </Grid>
+        <Grid item className="rightbar" xs={12} sm={2}>
+          <TourCard
+            setSelectedItems={setSelectedItems}
+            selectedItems={selectedItems}
+            selectedIndex={selectedIndex}
+          />
+        </Grid>
+      </Grid>
+    </div>
   );
 }
 
-export default Test;
+export default CreatePlanner;
